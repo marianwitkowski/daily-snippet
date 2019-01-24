6181524b79f720e024d2b861c86322eeac9c7b6d
Marian Witkowski <marian.witkowski@gmail.com>
2019-01-24 11:46:51 +0100

    Update PrintNTimes.java

--- PrintNTimes.java
+++ PrintNTimes.java
@@ -10,7 +10,16 @@ public class PrintNTimes
 {
   public static void main(String[] args)
   {
+	// example #1
 	String repeatedHyphens = new String(new char[20]).replace('\0', '-');
+	System.out.println(repeatedHyphens);
+	
+	// example #2
+	repeatedHyphens = String.format("%0" + 20 + "d", 0).replace("0", "-");
+	System.out.println(repeatedHyphens);
+	
+	// example #3 - Java 8 usage
+	repeatedHyphens = String.join("", java.util.Collections.nCopies(20, "-"));
 	System.out.println(repeatedHyphens);
   }
 }
